6ac36a9af18f5c2aff0f5ceab261eca7b3484854
jarvisassistant19-pixel <jarvisassistant19@gmail.com>
2026-02-15 11:46:40 +0530

    modified req.txt and server.py

--- server.py
+++ server.py
@@ -291,4 +291,6 @@ def handle_config():
         return jsonify({"target_url": "https://www.instagram.com"}), 200
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
